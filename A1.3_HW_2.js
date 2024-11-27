52b0c966238c09c937afefa43f343207c0266b6a
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-11-27 05:57:31 +0000

    Pratice And Class Work In neoCamp

--- A1.3_HW_2.js
+++ A1.3_HW_2.js
@@ -11,17 +11,9 @@ let fruits3 = "Orange"
 let fruits = fruits1 + ",  " + fruits2 + ",  "+ fruits3
 console.log(fruits) 
 
+
 console.log("---- ---- -----")
 console.log("1.2")
-
-let Price1 = 5.99
-let Price2 = 9.99
-let Price3 = 12.99
-
-console.log (Price1 , Price2 , Price3)
-
-console.log("---- ---- -----")
-console.log("1.3")
 
 let friend1 = "Sarah"
 let friend2 = "Alex"
@@ -33,6 +25,16 @@ let greet3 ="Hey " + friend3
 
 console.log( greet1, "\n",greet2 ,"\n" , greet3)
 
-
 console.log("---- ---- -----")
 console.log("1.3")
+
+let Price1 = 5.99
+let Price2 = 9.99
+let Price3 = 12.99
+
+console.log ("The Price of frist item is ",Price1)
+console.log ("The Price of second item is ",Price1)
+console.log ("The Price of third item is ",Price1)
+
+
+
---
+++ A1.4_CW.js
@@ -0,0 +1,48 @@
+console.log("A1.4_CW")
+console.log("---- ---- ---- ----")
+console.log("Exeresice 1")
+console.log("---- ---- ---- ----")
+console.log("1.1")
+
+
+let movieOne = "War"
+let movieOneActor1 = "Hritik Roshan"
+let movieOneActor2 = "Tiger Shroff"
+let movieOneActor3 = "Vaani Kapoor"
+let movieOneRating = 6.5
+let movieOneDurationInMinutes = 154
+let movieOneRealeaseYear = 2019
+let movieOneDescription = "An Indian Soldier is assigned to eliminate to eliminate his former mentor, who has gone rogue."
+
+console.log("Movie Detais")
+console.log("Movie: " + movieOne)
+console.log("Rating: " + movieOneRating)
+console.log("Duration: "+ movieOneDurationInMinutes)
+console.log("Realeased: " + movieOneRealeaseYear)
+console.log("Actors: " + movieOneActor1 + ", " + movieOneActor2 + ", "+ movieOneActor3)
+console.log("Plots: " + movieOneDescription)
+
+console.log("---- ---- ---- ----")
+console.log("Exrecise")
+console.log("---- ---- ---- ----")
+console.log("2.1")
+
+let movieTwo = "Baahuhbali"
+let movieTwoActor1 = "Prabas"
+let movieTwoActor2 = "Rana Daggubati"
+let movieTwoActor3 = "Anushka Shetty"
+let movieTwoRating = 8.2
+let movieTwoDurationInMinutes = 159
+let movieTwoRealeseYear = 2015
+let movietwoDescription = "In the kingdom of Mahishmati, a flerce warrior risee to defy an empire ans save the nationa"
+
+console.log("Movie Details")
+console.log("Movie :" + movieTwo)
+console.log("Rating: ", movieTwoRating)
+console.log("Duration: ", movieTwoDurationInMinutes)
+console.log("Realeased: ",movieTwoRealeseYear)
+console.log("Actors: " + movieTwoActor1 + ", "+ movieTwoActor2 + " ," + movieTwoActor3)
+console.log("Plot: ", movietwoDescription)
+
+
+
