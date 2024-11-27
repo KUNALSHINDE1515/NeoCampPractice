21bb65bba9a9007682a3575eff966c145cf7c508
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-11-27 05:25:58 +0000

    A1.3_HW_2.js

--- A1.3_HW_2.js
+++ A1.3_HW_2.js
@@ -1,3 +1,4 @@
+
 console.log("A1.3 | Exerrcise 1")
 
 console.log("---- ---- ---- ----")
@@ -8,4 +9,30 @@ let fruits3 = "Orange"
 
 
 let fruits = fruits1 + ",  " + fruits2 + ",  "+ fruits3
-console.log(fruits)
+console.log(fruits) 
+
+console.log("---- ---- -----")
+console.log("1.2")
+
+let Price1 = 5.99
+let Price2 = 9.99
+let Price3 = 12.99
+
+console.log (Price1 , Price2 , Price3)
+
+console.log("---- ---- -----")
+console.log("1.3")
+
+let friend1 = "Sarah"
+let friend2 = "Alex"
+let friend3 =  "Jessica"
+
+let greet1 ="Hey " + friend1
+let greet2 ="Hey " + friend2
+let greet3 ="Hey " + friend3
+
+console.log( greet1, "\n",greet2 ,"\n" , greet3)
+
+
+console.log("---- ---- -----")
+console.log("1.3")
