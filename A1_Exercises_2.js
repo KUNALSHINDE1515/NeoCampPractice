c696af11dfc763d1d1de4829f65c208b4feba8b1
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-12-02 11:05:03 +0000

    Exercise 1 done

---
+++ A1_Exercises_2.js
@@ -0,0 +1,36 @@
+console.log("Lesson A1")
+console.log("Exrecise_2")
+console.log(" ")
+console.log("---- ----- ----")
+let totalItems = 3 
+console.log("Inventory Details " + '(' ,totalItems ,')')
+
+console.log(" ")
+
+let itemOneName = "T-shirt"
+let itemOneQuantity = 10
+let itemOnePrice = 495.99
+
+let itemTwoName = "Jeans"
+let itemTwoQuantity = 5
+let itemTwoPrice = 2995.99
+
+let itemThreeName = "Shoes"
+let itemThreeQuantity = 8
+let itemThreePrice = 1499.99
+
+console.log("Item 1 :")
+console.log("Name : ", itemOneName)
+console.log("Quatity :", itemOneQuantity)
+console.log("Price : Rs.", itemOnePrice)
+console.log(" ")
+console.log("Item 2 :")
+console.log("Name : ", itemTwoName)
+console.log("Quatity :", itemTwoQuantity)
+console.log("Price : Rs.", itemTwoPrice)
+
+console.log(" ")
+console.log("Item 3 :")
+console.log("Name : ", itemThreeName)
+console.log("Quatity :", itemThreeQuantity)
+console.log("Price : Rs.", itemThreePrice)
---
+++ A1_Final_Assignment.js
@@ -0,0 +1,56 @@
+console.log("Final Assignment");
+console.log("A1");
+console.log("---- ---- ----");
+let totalEvents = 3;
+console.log(" Total Events ", totalEvents);
+
+console.log("");
+
+let eventOneTittle = "Music Concert";
+
+let eventOneDate = "August 15, 2023";
+let eventOneTime = "7.00 PM";
+let eventOneTicketsSold = 150;
+let eventOneTicketPrice = 2599;
+
+console.log("Events One : ");
+console.log("Title : ", eventOneTittle);
+console.log("Date : ", eventOneDate);
+console.log("Time : ", eventOneTime);
+console.log("Tickets Sold :", eventOneTicketsSold);
+console.log("Ticket Price : ", eventOneTicketPrice);
+
+console.log("---- ---- ---- ----");
+
+console.log("");
+
+let eventTwwoTittle = "Art Exhibition";
+let eventTwoDate = "September 10 ,2023";
+let eventTwoTime = "11:00 AM";
+let eventTwoTicketSold = 80;
+let eventTwoTicketPrice = 159;
+
+console.log(" ");
+
+console.log("Events Two : ");
+console.log("Title : ", eventTwwoTittle);
+console.log("Date : ", eventTwoDate);
+console.log("Time : ", eventTwoTime);
+console.log("Tickets Sold :", eventTwoTicketSold);
+console.log("Ticket Price : ", eventTwoTicketPrice);
+
+console.log("---- ---- ---- ----");
+console.log("");
+
+let eventThreeTittle = "Sports Tournaments";
+let eventThreeDate = "October 5, 2023";
+let eventThreeTime = "2.30PM";
+let eventThreeTicketSold = 200;
+let eventThreeTicketPrice = 1299;
+
+console.log("Events Three : ");
+console.log("Title : ", eventTwwoTittle);
+console.log("Date : ", eventThreeDate);
+console.log("Time : ", eventThreeTime);
+console.log("Tickets Sold :", eventTwoTicketSold);
+console.log("Ticket Price : ", eventThreeTicketPrice);
