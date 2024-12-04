277bf2e50888786839feed3a175136606e40b783
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-12-04 04:41:59 +0000

    A2.1_HW_2.js

---
+++ A2.1_HW_2.js
@@ -0,0 +1,84 @@
+console.log("A2.1_HW_2.1");
+console.log("Exercise  1");
+console.log("---- ---- ---- ----");
+
+let numberOne = 10;
+let numberTwo = 5;
+
+console.log("1.1");
+let sum = numberOne + numberTwo;
+console.log("Numbers :", numberOne, numberTwo);
+console.log("Sum : ", sum);
+
+console.log("---- ---- ---- ----");
+console.log("1.2");
+let difference = numberOne - numberTwo;
+console.log("Numbers :", numberOne, numberTwo);
+console.log("difference : ", difference);
+
+console.log("---- ---- ---- ----");
+console.log("1.3");
+let product = numberOne * numberTwo;
+console.log("Numbers :", numberOne, numberTwo);
+console.log("product : ", product);
+
+console.log("---- ---- ---- ----");
+console.log("1.4");
+let division = numberOne / numberTwo;
+console.log("Numbers :", numberOne, numberTwo);
+console.log("division : ", division);
+console.log("---- ---- ---- ----");
+console.log("1.5");
+let modulo = numberOne % numberTwo;
+console.log("Numbers :", numberOne, numberTwo);
+console.log("modulo : ", modulo);
+
+console.log("---- ---- ---- ----");
+console.log("Exercise 2");
+
+let numberThree = 25;
+
+console.log("---- ---- ---- ----");
+console.log("2.1");
+
+let sumAll = numberOne + numberTwo + numberThree;
+console.log("Number One :", numberOne);
+console.log("Number Two :", numberTwo);
+console.log("Number Three :", numberThree);
+console.log("sum of All : ", sumAll);
+
+console.log("---- ---- ---- ----");
+console.log("2.2");
+
+let AddSub = numberOne + numberTwo - numberThree;
+console.log("Number One :", numberOne);
+console.log("Number Two :", numberTwo);
+console.log("Number Three :", numberThree);
+console.log("Add And Sub : ", AddSub);
+
+console.log("---- ---- ---- ----");
+console.log("2.3");
+
+let MultiAdd = numberOne * numberTwo + numberThree;
+console.log("Number One :", numberOne);
+console.log("Number Two :", numberTwo);
+console.log("Number Three :", numberThree);
+console.log("Multi Add : ", MultiAdd);
+
+console.log("---- ---- ---- ----");
+console.log("2.4");
+
+let DivideAdd = numberThree / numberOne + numberTwo;
+console.log("Number One :", numberOne);
+console.log("Number Two :", numberTwo);
+console.log("Number Three :", numberThree);
+console.log("Divide and Add : ", DivideAdd);
+
+console.log("---- ---- ---- ----");
+console.log("2.4");
+
+let Avearage = (numberThree + numberOne + numberTwo) / 3;
+console.log("Number One :", numberOne);
+console.log("Number Two :", numberTwo);
+console.log("Number Three :", numberThree);
+console.log("Avearage : ", Avearage);
