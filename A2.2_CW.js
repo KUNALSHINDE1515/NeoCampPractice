adf51e4b01e7fd44622556f41926c22edbacca09
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-12-08 13:38:23 +0000

    practice class

---
+++ A2.2_CW.js
@@ -0,0 +1,27 @@
+console.log("A2.2_CW");
+console.log("---- ---- ---- ----");
+console.log("exercise 1");
+
+console.log("1.1");
+
+let isRohitAge = 22;
+let isRahul = 23;
+
+let isRohitOlder = isRohitAge > isRahul;
+console.log("Is Rohit older than Rahul ?", isRohitOlder);
+
+console.log("---- ---- ---- ----");
+
+console.log("1.2");
+
+let isRohitYonger = isRohitAge < isRahul;
+console.log("Is Rohit yonger than rahul ?", isRohitYonger);
+
+console.log("---- ---- ---- ----");
+
+console.log("---- ---- ---- -----");
+console.log("1.3");
+
+let romaage = 22;
+let isRohitEqualRoma = isRohitAge === romaage;
+console.log("Are Rohit and Roma of the same age ?", isRohitEqualRoma);
