5989ea4a9a483cf36e55d9ef19f74935a203bd73
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-12-09 07:59:32 +0000

    Second Exercise completed

---
+++ A2.2_HW_2.js
@@ -0,0 +1,91 @@
+console.log("---- ----- ----- -----")
+console.log("A2.2_HW_2")
+console.log("Exercise 1")
+console.log("---- ----- ----- -----")
+console.log('1.1')
+
+let rajeshRating  = 8.2
+ let amitRating = 7.9
+ let isRajeshHigherRated = rajeshRating > amitRating
+
+console.log("is rajesh higher rated than amit? ", isRajeshHigherRated)
+
+
+console.log("---- ----- ----- -----")
+console.log('1.2')
+
+
+
+let rajeshCollection = 250
+let amitCollection = 350
+
+ let isRajeshHigherCollection = rajeshCollection > amitCollection
+console.log("Does Rajedh have a higher superhit movie collection than amit ?", isRajeshHigherCollection)
+
+
+console.log("---- ----- ----- -----")
+console.log('1.3')
+
+let RajeshIncome = 50
+let amitIncome = 80
+let isRajeshLessIncome = RajeshIncome < amitIncome
+
+console.log("Does rajesh have a lower yearly income than amit ? " ,isRajeshLessIncome)
+
+
+console.log("---- ----- ----- -----")
+console.log('1.4')
+
+let rajeshAds = 10
+let amitAds = 8
+let isRajeshMoreAds = rajeshAds > amitAds
+console.log("Has Rajesh Done more ads than Amit ? " , isRajeshMoreAds)
+console.log("---- ----- ----- -----")
+console.log('1.5')
+
+let rajeshFans = 2_000_000
+let amitFans = 3_500_000
+
+let isRajeshMoreFans = rajeshFans > amitFans
+console.log("Does rajesh have more fan than Amit? ", isRajeshMoreFans)
+
+
+
+console.log("---- ----- ----- -----")
+console.log("Exercise 2")
+console.log("---- ----- ----- -----")
+console.log('2.1')
+
+let tShirtPrice = 799
+let jeansPrice = 599
+
+let budget = 1500
+
+let areBothUnderbudget = (tShirtPrice + jeansPrice) < budget
+
+console.log("Can I buy both a T- shirt and jeans within my budget ?" , areBothUnderbudget)
+
+console.log("---- ----- ----- -----")
+console.log('2.2')
+
+let mathScore = 85 
+let englishScore  = 92
+let physicsScore = 78
+
+let minumalAverage = 85
+
+let averageScore = (mathScore + englishScore + physicsScore) / 3
+
+let isQualified = minumalAverage < averageScore
+
+console.log("Am i qualififes for the olympiad ?" , isQualified)
+
+console.log("---- ----- ----- -----")
+console.log('2.3')
+
+let productQuantity = 60
+let isDivisibleBy5 = productQuantity % 5 === 0
+let isDivisibleBy10 = productQuantity % 10 === 0
+
+console.log("Can the Product quantity be sold in batches of 5 ? ",isDivisibleBy5)
+console.log("Can the Product quantity be sold in batches of 10 ? ",isDivisibleBy10)
