029556fc7281305cd60465566dfcc7f8ee885be4
Kunal Shinde <124497605+KUNALSHINDE1515@users.noreply.github.com>
2024-12-09 06:53:39 +0000

    Second Exercise Completed Hw

---
+++ A2.2_HW_1.js
@@ -0,0 +1,102 @@
+console.log("---- - --- ---- -----")
+console.log("excercise 1")
+console.log("---- ----- ---- -----")
+console.log("1.1")
+
+
+
+let akshayTime = 10.75
+let raviTime = 10.92
+
+let isAkshayFaster = akshayTime < raviTime
+console.log("Is Akshay faster tthan Ravi in the 100m sprint ? ", isAkshayFaster)
+
+
+console.log("----- ----- ------ ------")
+console.log("1.2")
+
+let akshayTime400 = 46.21
+let raviTime400 = 47.05
+
+let isAkshayFaster400 = akshayTime400 > raviTime
+
+console.log("Is Akshay faster than ravi in the 400m race ?" ,isAkshayFaster400)
+
+
+console.log("----- ----- ------ ------")
+console.log("1.3")
+
+
+
+let akshayDistance = 7.83
+let raviDistance = 7.68
+
+
+let isakshayLongerJump = raviDistance < akshayDistance
+
+console.log("Did Akshay make a longer jump than ravi ? ", isakshayLongerJump)
+
+console.log("----- ----- ------ ------")
+console.log("1.4")
+
+let akshayDistanceShortPut = 19.75
+let raviDistanceShortPut = 18.92
+
+let isakshayLongerThrow = akshayDistanceShortPut > raviDistanceShortPut
+
+console.log("Did askshay make a longer throw than ravi ?", isakshayLongerThrow)
+
+
+console.log("----- ----- ------ ------")
+console.log("1.5")
+
+let akshayHeight = 2.05
+let raviheight = 1.98
+
+let isakshayHigherJummper = akshayHeight > raviheight
+
+console.log("Did Akshay jump higher than ravi ?", isakshayHigherJummper)
+
+
+console.log("---- - --- ---- -----")
+console.log("excercise 2")
+console.log("---- ----- ---- -----")
+console.log("2.1")
+
+
+let milkPrice = 199
+let breadPrice = 299
+
+let  budget = 300
+let arebothUnderBudget = (milkPrice + breadPrice) < budget
+
+console.log("Can I buy both milk and Bread within my budget ?", arebothUnderBudget)
+
+console.log("---- ----- ----- -----")
+console.log("2.2")
+
+
+let gangaCriketClubScore = 78
+let yamunaCriketClubScore =  63
+let narmadaCriketClubScore = 52
+let godavariCriketclubScore = 72
+
+
+let minimunAverage = 60
+
+let avearageScore = (gangaCriketClubScore + yamunaCriketClubScore + narmadaCriketClubScore + godavariCriketclubScore) / 4
+// console.log(avearageScore)
+
+let isSelected = avearageScore > minimunAverage
+console.log("Am I  selected for the criket camp? ", isSelected)
+
+console.log("---- ----- ----- -----")
+console.log("2.3")
+
+let numberOfSudents = 72
+let isDivisibleBy4 = numberOfSudents % 4 === 0
+let isDivisibleBy9 = numberOfSudents % 9 === 0
+
+
+console.log("Can the students be divided into groups of 4 ?" , isDivisibleBy4)
+console.log("Can the students be divided into groups of 9 ?" , isDivisibleBy9)
